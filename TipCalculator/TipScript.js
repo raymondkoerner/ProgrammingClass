c46be47cfd782d4022598038801778ab4a6df678
raymondkoerner <theraymondchannel@gmail.com>
2022-01-19 19:45:28 -0500

    Update TipScript.js

--- TipCalculator/TipScript.js
+++ TipCalculator/TipScript.js
@@ -1,3 +1,20 @@
-.....Delete these lines.....
-Ray, you need to fill this in with the code you get from the Tip Calculator video....
-.....Delete these lines ....
+
+/grabbing pieces of the HTML page to store as a later use
+let directions = document.getElementById('directions');
+let input = document.getElementById('input')
+let button = document.getElementById('button')
+let output = document.getElementById("output");
+
+// change the text on the page for the directions. 
+Directions.innerHtml = "enter the cost of the meal . /npress'calculate to get tip"
+
+/"Write the fuction that will calculate the tip amount from the cost of the meal at 155"/
+function calculate Tip ()
+ {
+    let cost = parseFloat(input.value);
+  let percent = 15;
+  let tip = cost percent/100;
+  Tip = Tip.Tofixed(2);
+  Let outputText = "The tip at " + percent + "S will be S " + tip;
+output.innerHTML = outputText;
+}
