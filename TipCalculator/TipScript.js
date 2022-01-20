025803d977d4d9c286ca699d3bd383ee748ff139
raymondkoerner <theraymondchannel@gmail.com>
2022-01-19 20:42:42 -0500

    Update TipScript.js

--- TipCalculator/TipScript.js
+++ TipCalculator/TipScript.js
@@ -1,20 +1,24 @@
 
-/grabbing pieces of the HTML page to store as a later use
+//grabbing pieces of the HTML page to store as a later use
 let directions = document.getElementById('directions');
-let input = document.getElementById('input')
-let button = document.getElementById('button')
+let input = document.getElementById('input');
+let button = document.getElementById('button');
 let output = document.getElementById("output");
 
-// change the text on the page for the directions. 
-Directions.innerHtml = "enter the cost of the meal . /npress'calculate to get tip"
+// change the text on the page for the directions.
+directions.innerHTML = "Enter the cost of the meal. \nPress 'calculate' to get tip.";
 
-/"Write the fuction that will calculate the tip amount from the cost of the meal at 155"/
-function calculate Tip ()
+
+/*Write the fuction that will calculate the tip amount from the
+  cost of the meal at 155*/
+function calculateTip ()
  {
     let cost = parseFloat(input.value);
-  let percent = 15;
-  let tip = cost percent/100;
-  Tip = Tip.Tofixed(2);
-  Let outputText = "The tip at " + percent + "S will be S " + tip;
-output.innerHTML = outputText;
+    let percent = 15;
+    let tip = cost * percent/100;
+    tip = tip.toFixed(2);
+    let outputText = "The tip at " + percent + "% will be $" + tip;
+    output.innerHTML = outputText;
 }
+
+button.onclick = calculateTip;
