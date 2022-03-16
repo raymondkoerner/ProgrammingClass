3847302ee9cda49cb242f8cf7a4018c010e757ef
raymondkoerner <theraymondchannel@gmail.com>
2022-03-16 17:56:07 -0400

    Add files via upload

---
+++ Python/Lab_Lists.py
@@ -0,0 +1,16 @@
+# Raymond Koerner -  On my honor as a student, I have neither given nor received any unauthorized aid on this assignment.
+
+#Lab_Lists.py
+
+#Task 1
+names=["raymond j. koerner","Max","Thomas","Matthew","Bryce","Jay"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+
+print(f'I have two brothers: {names[1]} and {names[2]}')
+print(f'My friends are : {names[3]}, {names[4]} and {names[5]}')
+print("My name is " + names[0].title())
