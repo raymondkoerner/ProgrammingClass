b766aa5db4d84c54be1eb4e8264ce67ab07be3ac
raymondkoerner <theraymondchannel@gmail.com>
2022-03-27 12:16:25 -0400

    Add files via upload

--- Python/magic8.py
+++ Python/magic8.py
@@ -2,47 +2,46 @@ import random
 
 # Raymond Koerner -  On my honor as a student, I have neither given nor received any unauthorized aid on this assignment.
 
-name=input("what is your name? \n")
-question=input("what is your question? \n")
+#magic8.py
+name=input("What is your name?  \n")
+question=input("What is your question?  Must be a yes or no question.  \n")
 answer=""
-answers=["yes definently", 
-         "It is decidedly so",
-         "very doubtful" ,
-         "reply hazy" ,  
-         "try again" ,
-         "ask again later" , 
-         "better not tell you now" ,
-         "my sources say no" ,
-        "out look not so good"]
+answers=["yes, definitely",
+         "it is decidedly so",
+         "without a doubt",
+         "reply hazy, try again",
+         "ask again later",
+         "better not tell you now",
+         "my sources say no",
+         "outlook not so good",
+         "very doubtful"]
 
 random_number=random.randint(1,9)
+#print("Random number is: " + str(random_number))
 
 if(name != ""):
-    print(name + " asks : " + question)
+    print(name.title() + " asks: " + question.title())
 else:
     print("Question: " + question)
 
 if(question != ""):
-    if (random_number == 1):
-        print("Magic 8-Ball Answer: " + answers[0])
-    elif (random_number == 2):
-        print("Magic 8-Ball Answer: " + answers[1])
+    if random_number == 1:
+        print(f'Magic 8-Ball\'s answer:  {answers[0].title()}')
+    elif random_number == 2:
+        print(f'Magic 8-Ball\'s answer:  {answers[1].title()}')
     elif random_number == 3:
-        print("Magic 8-Ball Answer: " + answers[2])
+        print(f'Magic 8-Ball\'s answer:  {answers[2].title()}')
     elif random_number == 4:
-        print("Magic 8-Ball Answer: " + answers[3])
+        print(f'Magic 8-Ball\'s answer:  {answers[3].title()}')
     elif random_number == 5:
-        print("Magic 8-Ball Answer: " + answers[4])
+        print(f'Magic 8-Ball\'s answer:  {answers[4].title()}')
     elif random_number == 6:
-        print("Magic 8-Ball Answer: " + answers[5])
+        print(f'Magic 8-Ball\'s answer:  {answers[5].title()}')
     elif random_number == 7:
-        print("Magic 8-Ball Answer: " + answers[6])
+        print(f'Magic 8-Ball\'s answer:  {answers[6].title()}')
     elif random_number == 8:
-        print("Magic 8-Ball Answer: " + answers[7])
+        print(f'Magic 8-Ball\'s answer:  {answers[7].title()}')
     elif random_number == 9:
-        print("Magic 8-Ball Answer: " + answers[8])
+        print(f'Magic 8-Ball\'s answer:  {answers[8].title()}')
 else:
-    print("You need to ask a question.")
-     
-     
-        
+    print("You need to enter a question.")
