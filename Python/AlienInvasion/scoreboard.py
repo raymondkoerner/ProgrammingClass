475d8f93a815c08e10714283139b311843fc3623
raymondkoerner <theraymondchannel@gmail.com>
2022-05-25 10:45:47 -0400

    Add files via upload

--- Python/AlienInvasion/scoreboard.py
+++ Python/AlienInvasion/scoreboard.py
@@ -9,24 +9,26 @@ class Scoreboard:
     """A class to report scoring information."""
 
     def __init__(self, ai_game):
-        """Initialize scorekeeping """
+        """Initialize scorekeeping attributes."""
         self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
 
+        # Font settings for scoring information.
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
+        # Prepare the initial score images.
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
-        self.prep_ships()
+        self.prep_ship()
 
     def prep_score(self):
-        rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        """Turn the score into a rendered image."""
+        score_str = "{:,}".format(self.stats.score)
         self.score_image = self.font.render(score_str, True,
                 self.text_color, self.settings.bg_color)
 
@@ -35,18 +37,8 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
-    def prep_high_score(self):
-        high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True,
-                self.text_color, self.settings.bg_color)
-
-        # Center the high score at the top of the screen.
-        self.high_score_rect = self.high_score_image.get_rect()
-        self.high_score_rect.centerx = self.screen_rect.centerx
-        self.high_score_rect.top = self.score_rect.top
-
     def prep_level(self):
+        """Turn the level into a rendered image."""
         level_str = str(self.stats.level)
         self.level_image = self.font.render(level_str, True,
                 self.text_color, self.settings.bg_color)
@@ -56,7 +48,7 @@ class Scoreboard:
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
-    def prep_ships(self):
+    def prep_ship(self):
         """Show how many ships are left."""
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
@@ -65,7 +57,21 @@ class Scoreboard:
             ship.rect.y = 10
             self.ships.add(ship)
 
+
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score_str = "{:,}".format(self.stats.high_score)
+        self.high_score_image = self.font.render(high_score_str, True,
+                self.text_color, self.settings.bg_color)
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+
     def check_high_score(self):
+        """Check to see if there's a new high score."""
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
